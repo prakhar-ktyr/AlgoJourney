1f7146f47c5ea2ff7aca02eb7b7f4326798d9d5f
Prakhar Katiyar <prakhar_katiyar@outlook.com>
2026-05-27 07:15:06 +0000

    docs: update links for array rotation problems in Striver's sheet

--- client/src/data/striversSheet.js
+++ client/src/data/striversSheet.js
@@ -334,8 +334,8 @@ const STRIVERS_SHEET = [
           {
             title: "Left Rotate an array by one place",
             difficulty: "Easy",
-            lcLink: "https://leetcode.com/problems/rotate-array/",
-            gfgLink: "https://bit.ly/3dnn9vC",
+            // lcLink: "https://leetcode.com/problems/rotate-array/",
+            gfgLink: "https://www.geeksforgeeks.org/problems/cyclically-rotate-an-array-by-one2614/1",
           },
           {
             title: "Left rotate an array by D places",
